ea2b2f6120ca8d22aeead8798c05642f7c9cb641
陈胜勇 <chensy@tuya.com>
2020-03-11 12:55:23 +0800

    spring mvc环境搭建

---
+++ spring-util/src/main/java/controller/HelloController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * @author : chensy
+ * Date : 2020-03-09 21:09
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/toIndex")
+    public String toIndex(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "index";
+    }
+}
