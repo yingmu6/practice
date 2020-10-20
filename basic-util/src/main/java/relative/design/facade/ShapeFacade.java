9ad2604c4333dec88290a4ac208971209f892022
陈胜勇 <chensy@tuya.com>
2020-10-20 09:34:20 +0800

    设计模式之——门面模式、适配器模式

---
+++ basic-util/src/main/java/relative/design/facade/ShapeFacade.java
@@ -0,0 +1,24 @@
+package relative.design.facade;
+
+/**
+ * 形状门面（通过门面来与内部系统交互）
+ * @author : chensy
+ * Date : 2020/10/19 下午10:39
+ */
+public class ShapeFacade {
+    private Rectangle rectangle;
+    private Round round;
+
+    public ShapeFacade(){
+        rectangle = new Rectangle();
+        round = new Round();
+    }
+
+    public void drawRectangle() {
+        rectangle.draw();
+    }
+
+    public void drawRound() {
+        round.draw();
+    }
+}
