f415229bb90c882bc9f5d435e4c85fb4bb6a77f1
陈胜勇 <chensy@tuya.com>
2020-11-16 23:57:31 +0800

    netty使用

---
+++ basic-util/src/main/java/relative/netty/test3/Server.java
@@ -0,0 +1,39 @@
+package relative.netty.test3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Netty服务端
+ * @author : chensy
+ * Date : 2020/11/16 下午8:16
+ */
+public class Server {
+    public static void main(String[] args) {
+        basic();
+    }
+
+    public static void basic() {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        System.out.println("server init channel");
+                    }
+                });
+        serverBootstrap.bind(8000);
+    }
+
+    /**
+     * NioEventLoopGroup：主要管理eventLoop的生命周期。
+     * https://www.jianshu.com/p/e577803f0fb8
+     */
+}
