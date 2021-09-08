e16592c1ee9aedfac10b9fe20c3c01a50648e416
chenshengyong <chensy@tuya.com>
2021-09-08 23:24:50 +0800

    test

--- basic-util/src/main/java/relative/basic/abstracttest/Animal.java
+++ basic-util/src/main/java/relative/basic/abstracttest/Animal.java
@@ -4,14 +4,14 @@ package relative.basic.abstracttest;
  * @author : chensy
  * Date : 2020/8/24 上午10:07
  */
-public abstract class Animal {
+public abstract class Animal { //
     abstract void cry();
 
     /**
      * 1）向上转型
      * 2）抽象类实现多态
      */
-    public static void main(String[] args) { //
+    public static void main(String[] args) {
         Animal animal = new Dog();
         animal.cry();
 
