85c7eb8923277114048dd99f64537bedaac40dbd
chenshengyong <chensy@tuya.com>
2021-12-25 22:59:15 +0800

    test

--- basic-util/src/main/java/relative/basic/abstracttest/Dog.java
+++ basic-util/src/main/java/relative/basic/abstracttest/Dog.java
@@ -4,7 +4,7 @@ package relative.basic.abstracttest;
  * @author : chensy
  * Date : 2020/8/24 上午10:08
  */
-public class Dog extends Animal{
+public class Dog extends Animal { //test
     @Override
     void cry() {
         System.out.println("狗叫");
