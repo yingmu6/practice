614ea050de51b9feb214f1d1eae2814669f82226
chenshengyong <chensy@tuya.com>
2022-05-07 23:41:01 +0800

    stream排序编写

--- practice-jc/src/main/java/relative/java8/StreamTest.java
+++ practice-jc/src/main/java/relative/java8/StreamTest.java
@@ -1,9 +1,12 @@
 package relative.java8;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
+import org.springframework.util.StringUtils;
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author : chensy
@@ -30,6 +33,49 @@ public class StreamTest {
 //        Optional option = Optional.empty();
 //        System.out.println(option.isPresent());
 //        System.out.println(isNeedNotice);
+
+
+        orderPerson();
+    }
+
+    // 排序
+    public static void orderPerson() {
+
+        Person person = new Person();
+        person.setName("张三");
+        person.setAge(18);
+        person.setGmtCreate(189L);
+
+        Person person2 = new Person();
+        person2.setName("李李");
+        person2.setAge(13);
+        person2.setGmtCreate(188L);
+
+        Person person3 = new Person();
+        person3.setName("王王");
+        person3.setAge(19);
+        person3.setGmtCreate(187L);
+
+        List<Person> personList = Lists.newArrayList();
+        personList.add(person);
+        personList.add(person2);
+        personList.add(person3);
+
+        List<Person> newList = personList.stream()
+                .sorted(Comparator.comparing(Person::getAge).reversed())
+                .collect(Collectors.toList());
+        System.out.println("第一个：" + newList);
+
+        List<Person> newList2 = personList.stream()
+                .filter(x -> !StringUtils.isEmpty(x.getName()))
+                .sorted(Comparator.comparing(Person::getName).reversed()) //降序
+                .collect(Collectors.toList());
+        System.out.println("第二个：" + newList2);
+
+        List<Person> newList3 = personList.stream()
+                .sorted(Comparator.comparing(Person::getGmtCreate)) //自然排序
+                .collect(Collectors.toList());
+        System.out.println("第三个：" + newList3);
     }
 }
 
@@ -37,4 +83,5 @@ public class StreamTest {
 class Person {
     private String name;
     private int age;
+    private Long gmtCreate;
 }
