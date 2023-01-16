f8eb6e89f676a71a2c31ba9249a92ce9e28c00e3
ChenSy <chensy@cloudyigou.com>
2023-01-16 22:49:08 +0800

    CompletableFuture实践

--- practice-jc/src/main/java/relative/basic/array_test/ArrayTest.java
+++ practice-jc/src/main/java/relative/basic/array_test/ArrayTest.java
@@ -10,11 +10,23 @@ import com.alibaba.fastjson.JSON;
  */
 public class ArrayTest {
     public static void main(String[] args) {
+        changeValue();
+    }
 
+    public static void basicUse() {
         String[] sourceArr = {"11", "22", "33"};
         String[] targetArr = new String[4];
 
         System.arraycopy(sourceArr, 1, targetArr, 0, 2);
         System.out.println(JSON.toJSONString(targetArr)); //输出：["22","33",null,null]
+    }
+
+    public static void changeValue() {
+        String[] sourceArr = {"11", "22", "33"};
+        String str = sourceArr[1];
+
+        sourceArr[1] = "aaa"; //对数组的元素设置了新的值
+        System.out.println(sourceArr[1]);
+
     }
 }
---
+++ practice-jc/src/main/java/relative/basic/concurrent/future/CompletableFutureTest.java
@@ -0,0 +1,19 @@
+package relative.basic.concurrent.future;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @Author chenSy
+ * @Date 2023/01/16 22:28
+ * @Description
+ */
+public class CompletableFutureTest { //todo @pause 待实践
+    public static void main(String[] args) {
+        CompletableFuture future = new CompletableFuture();
+
+    }
+
+    public void basicUse() {
+
+    }
+}
