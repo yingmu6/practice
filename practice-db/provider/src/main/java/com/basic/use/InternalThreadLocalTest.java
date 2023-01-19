f01857cb1cd2851c8c4023bc6feb0acb64691054
ChenSy <chensy@cloudyigou.com>
2023-01-19 22:35:21 +0800

    InternalThreadLocal测试

---
+++ practice-db/provider/src/main/java/com/basic/use/InternalThreadLocalTest.java
@@ -0,0 +1,16 @@
+package com.basic.use;
+
+//import org.apache.dubbo.common.threadlocal.InternalThreadLocal;
+
+/**
+ * @Author chenSy
+ * @Date 2023/01/19 08:31
+ * @Description
+ */
+public class InternalThreadLocalTest {
+    public static void main(String[] args) {
+        //InternalThreadLocal<Integer> local
+    }
+
+
+}
