49b65d7c9cb227b141e130221c149e1bb5e8c4a0
ChenSy <chensy@cloudyigou.com>
2023-01-20 22:51:58 +0800

    test

--- practice-db/provider/src/main/java/com/basic/use/InternalThreadLocalTest.java
+++ practice-db/provider/src/main/java/com/basic/use/InternalThreadLocalTest.java
@@ -8,7 +8,7 @@ package com.basic.use;
  * @Description
  */
 public class InternalThreadLocalTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //test
         //InternalThreadLocal<Integer> local
     }
 
