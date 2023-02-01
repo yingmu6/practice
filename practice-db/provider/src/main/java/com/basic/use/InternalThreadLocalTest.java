7a182e4d882a7b3cac3815c31fee215ecc76fe44
ChenSy <chensy@cloudyigou.com>
2023-02-01 23:17:07 +0800

    2月flag

--- practice-db/provider/src/main/java/com/basic/use/InternalThreadLocalTest.java
+++ practice-db/provider/src/main/java/com/basic/use/InternalThreadLocalTest.java
@@ -7,7 +7,7 @@ package com.basic.use;
  * @Date 2023/01/19 08:31
  * @Description
  */
-public class InternalThreadLocalTest {
+public class InternalThreadLocalTest { //flag
     public static void main(String[] args) { //test
         //InternalThreadLocal<Integer> local
     }
