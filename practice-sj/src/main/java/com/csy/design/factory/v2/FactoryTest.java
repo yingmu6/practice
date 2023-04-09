be95cac933a360792e839693f67bf84dbde8618a
ChenSy <chensy@cloudyigou.com>
2023-04-09 22:07:46 +0800

    工厂模式测试

--- practice-sj/src/main/java/com/csy/design/factory/v2/FactoryTest.java
+++ practice-sj/src/main/java/com/csy/design/factory/v2/FactoryTest.java
@@ -1,10 +1,24 @@
 package com.csy.design.factory.v2;
 
+import org.junit.Test;
+
 /**
- * todo @csy-23/02/21 工厂模式待测试
  * @Author chenSy
  * @Date 2023/02/21 23:09
  * @Description
  */
 public class FactoryTest {
+
+    /**
+     * 工厂模式/场景1：工厂模式基本使用
+     */
+    @Test
+    public void test() {
+
+    }
+
+    /**
+     * 工厂模式/场景2：
+     */
+
 }
