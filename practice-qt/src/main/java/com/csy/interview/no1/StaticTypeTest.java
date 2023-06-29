3254d519d017783d94018baf03276ec1624e277c
shengyong.chen <csyGitHub>
2023-06-29 23:00:57 +0800

    Final、Static使用

---
+++ practice-qt/src/main/java/com/csy/interview/no1/StaticTypeTest.java
@@ -0,0 +1,48 @@
+package com.csy.interview.no1;
+
+/**
+ * @author chensy
+ * @date 2023/6/29
+ */
+public class StaticTypeTest {
+
+    /**
+     * static关键字_概述
+     * 1）We'll find out how we can apply the static keyword to variables, methods,
+     *    blocks, and nested classes, and what difference it makes.
+     *   （static关键字可以用于修饰变量、方法、代码块、内部类等）
+     *
+     * 2）In the Java programming language, the keyword static means that the particular member
+     *   belongs to a type itself, rather than to an instance of that type.
+     *   （static关键字意味着 指定成员归属于类型本身，而不是属于类型的实例）
+     *
+     * 3）Since static variables belong to a class, we can access them directly using the class name. So, we don't need any object reference.
+     *   （由于static归属于类，所以可以使用类名直接访问，而不需要用对象引用类型）
+     *
+     * 4）使用static字段的理由：
+     *   a）when the value of the variable is independent of objects（将变量值独立于对象）
+     *   b）when the value is supposed to be shared across all objects（支持所有对象共享变量）
+     *
+     * 参考链接：https://www.baeldung.com/java-static
+     */
+
+    /**
+     * 场景1：static变量
+     */
+
+    /**
+     * 场景2：static方法
+     */
+
+    /**
+     * 场景3：static代码块
+     */
+
+    /**
+     * 场景4：static内部类
+     */
+
+    /**
+     * 场景5：静态方法中与非静态方法的调用
+     */
+}
