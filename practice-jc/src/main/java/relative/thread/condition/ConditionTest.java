5beb0536ab1a0f3f59025bcc9d371ca8a08bd59d
shengyong.chen <csyGitHub>
2023-09-22 23:36:35 +0800

    CAS_测试

--- practice-jc/src/main/java/relative/thread/condition/ConditionTest.java
+++ practice-jc/src/main/java/relative/thread/condition/ConditionTest.java
@@ -1,5 +1,11 @@
 package relative.thread.condition;
 
+import org.junit.Test;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author chensy
  * @date 2023/9/21
@@ -10,6 +16,19 @@ public class ConditionTest {
      * Condition_测试
      *
      * 参考链接：
-     * a）https://zhuanlan.zhihu.com/p/312081597 Java并发之Condition详解
+     * a）https://zhuanlan.zhihu.com/p/312081597 Java并发之Condition详解（用例参考）
      */
+
+    private Lock lock = new ReentrantLock();
+
+    private Condition condition = lock.newCondition();
+
+    @Test
+    public void test_condition_basic() {
+
+    }
+
+    private void conditionWait() {
+
+    }
 }
--- practice-qt/src/main/java/com/csy/interview/no2/CASRelaTest.java
+++ practice-qt/src/main/java/com/csy/interview/no2/CASRelaTest.java
@@ -13,6 +13,9 @@ public class CASRelaTest {
 
     /**
      * CAS_测试
+     * 1）
+     *
+     * 2）Java中，使用AtomicStampedReference来解决ABA问题。它包含了属性值和戳记，只有两者都相等时，才进行更新。戳记相当于版本号
      *
      * 参考链接：
      * a）https://zhuanlan.zhihu.com/p/94762520 CAS原理讲解
@@ -21,12 +24,16 @@ public class CASRelaTest {
 
     private static AtomicInteger atomicInteger = new AtomicInteger(1);
 
-    private static AtomicStampedReference<Integer> asRef = new AtomicStampedReference<>(100, 0);
+    private static AtomicStampedReference<Integer> asRef = new AtomicStampedReference<>(1, 0);
 
     /**
      * 场景1：ABA问题
      *
-     * 举例说明：银行取款、存款
+     * 举例说明：银行取款、存款场景
+     * 1）开始账号中100元钱，现在两个线程T1、T2要取50元，都拿到100元然后更新为50元，正常情况下进行比较交换，只有一个线程能成功
+     *    当T1线程成功时，账号中的余额为50元，此时T2由于某种原因阻塞了，它还是持有扣减50元的任务。
+     * 2）此时T3线程存入50元，账户余额变为100元了，T2线程此时恢复时，发现账户余额为100元，所以能更新成功，最后余额变为50元
+     * 3）此场景中最终的余额为50元，但实际应该是100-50+50=100元，所以出现了ABA问题造成了影响（T2线程正确的场景下，是不能更新成功的）
      */
     @Test
     public void test_ABA_problem() throws InterruptedException {
@@ -80,25 +87,30 @@ public class CASRelaTest {
      * 场景2：解决ABA问题
      */
     @Test
-    public void test_resolve_ABA() throws InterruptedException {
-        resolve_ABA();
+    public void test_fix_ABA() throws InterruptedException {
+        fix_ABA();
 
         /**
          * 运行结果：
-         * Before sleep：stamp=0
-         * After sleep：stamp=0
-         * false
+         * 第一处比较交换的结果：true，stamp=1
+         * 第二处比较交换的结果：true，stamp=2
+         * 开始睡眠：stamp=1
+         * 结束睡眠：stamp=2
+         * 第三处比较交换的结果：false，stamp=2
          *
          * 结果分析：
          */
     }
 
-    public void resolve_ABA() throws InterruptedException {
+    public void fix_ABA() throws InterruptedException {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                asRef.compareAndSet(1, 2, asRef.getStamp(), asRef.getStamp() + 1);
-                asRef.compareAndSet(2, 1, asRef.getStamp(), asRef.getStamp() + 1);
+                boolean c1 = asRef.compareAndSet(1, 2, asRef.getStamp(), asRef.getStamp() + 1);
+                System.out.println("第一处比较交换的结果：" + c1 + "，stamp=" + asRef.getStamp());
+                boolean c2 = asRef.compareAndSet(2, 1, asRef.getStamp(), asRef.getStamp() + 1);
+                System.out.println("第二处比较交换的结果：" + c2 + "，stamp=" + asRef.getStamp());
+
             }
         });
 
@@ -106,16 +118,16 @@ public class CASRelaTest {
             @Override
             public void run() {
                 int stamp = asRef.getStamp();
-                System.out.println("Before sleep：stamp=" + stamp);
+                System.out.println("开始睡眠：stamp=" + stamp);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
-                System.out.println("After sleep：stamp=" + asRef.getStamp());
+                System.out.println("结束睡眠：stamp=" + asRef.getStamp());
                 boolean c3 = asRef.compareAndSet(1, 2, stamp, stamp + 1);
-                System.out.println(c3);
+                System.out.println("第三处比较交换的结果：" + c3 + "，stamp=" + asRef.getStamp());
             }
         });
 
