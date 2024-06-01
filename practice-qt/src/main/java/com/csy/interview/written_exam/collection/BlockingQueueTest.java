2be157003eaaf2d18b060ed79a7f07246d412c74
orange <csyGitHub>
2024-06-01 22:57:09 +0800

    kafka用例&面试用例

--- practice-qt/src/main/java/com/csy/interview/written_exam/collection/BlockingQueueTest.java
+++ practice-qt/src/main/java/com/csy/interview/written_exam/collection/BlockingQueueTest.java
@@ -20,6 +20,7 @@ public class BlockingQueueTest { //@MsY-Doing
      *    当阻塞队列是满时，往队列中添加元素的操作将会被阻塞
      *   （为什么需要使用BlockingQueue？好处是我们不需要关心什么时候需要阻塞线程，什么时候需要唤醒线程，因为BlockingQueue都一手给你包办好了。
      *    在concurrent包发布以前，多线程环境下，我们每个程序员都必须自己去控制这些细节，尤其还要兼顾效率和线程安全，而这会给我们的程序带来不小的复杂度。)
+     *
      * 2）阻塞队列有主要以下几种：
      *    ArrayBlockingQueue: 由数组结构组成的有界阻塞队列
      *    LinkedBlockingDeque: 由链表结构组成的有界(但大小默认值Integer>MAX_VALUE)阻塞队列
@@ -28,13 +29,19 @@ public class BlockingQueueTest { //@MsY-Doing
      *    SynchronousQueue:不存储元素的阻塞队列,也即是单个元素的队列
      *    LinkedTransferQueue:由链表结构组成的无界阻塞队列
      *    LinkedBlockingDeque:由了解结构组成的双向阻塞队列
+     *
      * 3）使用阻塞队列来解决生产者/消费者问题的时候，不再需要进行同步处理，这种思想在消息队列中有着广泛的应用。
+     *
      * 4）由于生产者与消费者是两个独立的并发体，他们之间是用缓冲区作为桥梁连接，生产者只需要往缓冲区里丢数据，就可以继续生产下一个数据，而消费者只需要从缓冲区了拿数据即可，这样就不会因为彼此的处理速度而发生阻塞。
      *
      * 关联点学习：
      * 1）Condition功能原理了解，以及与Object的wait、notify的差异（Doing）
+     *
      * 2）ReentrantLock中lockInterruptibly的功能用途了解（Doing）
-     * 3）
+     *
+     * 3）队列按数组和链表两种方式实现的代码实践（Doing）
+     *
+     * 4）链表的CRUD的相关代码实现（Doing）
      *
      * 参考链接：
      * a）https://bbs.huaweicloud.com/blogs/345349 生产者消费者模型概念以及优点
@@ -45,8 +52,8 @@ public class BlockingQueueTest { //@MsY-Doing
      * 场景1：使用ArrayBlockingQueue实现生产者、消费者模型
      */
     @Test
-    public void test_producer_consumer_by_ArrayBlockingQueue() throws IOException { //Doing
-        BlockingQueue<String> abq = new ArrayBlockingQueue<>(5);
+    public void test_producer_consumer_by_ArrayBlockingQueue() throws IOException { //Done
+        BlockingQueue<String> abq = new ArrayBlockingQueue<>(5); //指定队列的容量
 
         ExecutorService pool = Executors.newCachedThreadPool();
         pool.execute(new Producer(abq, 1000, "生产者"));
@@ -69,18 +76,26 @@ public class BlockingQueueTest { //@MsY-Doing
          *
          * 结果分析：
          * 1）从运行结果看，生产者：生产产品，产品在队列中增加，而消费者：消费产品，产品在队列中移除
+         *
          * 2）ArrayBlockingQueue中put和take的主要逻辑；
          *    2.1）put的主要逻辑：
-         *        a）会加锁进行添加元素，然后通过Condition的await，在队列元素慢的时候阻塞等待
+         *        a）会加锁进行添加元素，然后通过Condition的await，在队列元素满的时候阻塞等待，即count == items.length
          *        b）通过putIndex下标，依次从数组头部添加元素，当元素添加满后，putIndex置为0，又重新开始
          *        c）累加数组元素的计数count++，并唤醒等待的线程notEmpty.signal();
          *    2.2）take的主要逻辑：
-         *        a）会加锁进行添加元素，然后通过Condition的await，在队列元素慢的时候阻塞等待
+         *        a）会加锁进行添加元素，然后通过Condition的await，在队列元素空的时候阻塞等待，即count == 0
          *        b）通过takeIndex下标，依次从数组头部获取元素，当元素取完后，takeIndex置为0，又重新开始
          *        c）累减数组元素的计数count--，并唤醒等待的线程notFull.signal();
          *
+         * 3）ArrayBlockingQueue底层是通过数组实现的，通过putIndex、takeIndex两个下标实现队列的先进先出特性；
+         *    通过数组元素计数count来比较判断队列是否满或空，然后通过Condition的await()和signal()实现线程间的通信；
+         *    因为同一个阻塞队列可能被多个线程竞争，比如多个生产者、消费者线程，所以在添加、获取队列元素时都加锁了，内部使用ReentrantLock实现
+         *
          * 问题点答疑：
          * 1）ArrayBlockingQueue中Condition的notEmpty、notFull，是怎样的含义，感觉与字面含义不匹配？
+         *    解答：的确有些费解，不过这里使用的好处就是，一个变量两个地方使用。
+         *         看变量名的描述：变量 notFull，描述 Condition for waiting puts （用于等待put操作，即在队列不满时恢复，所以take操作时进行signal）
+         *                      变量 notEmpty，描述 Condition for waiting takes（用于等待take操作，即在队列不空时恢复，所以put操作时进行signal）
          */
     }
 
@@ -88,8 +103,8 @@ public class BlockingQueueTest { //@MsY-Doing
      * 场景2：使用PriorityBlockingQueue（优先级阻塞队列），实现生产者和消费者
      */
     @Test
-    public void test_producer_consumer_by_PriorityBlockingQueue() throws IOException {
-        PriorityBlockingQueue<PriorityProduct> pbq = new PriorityBlockingQueue<>();
+    public void test_producer_consumer_by_PriorityBlockingQueue() throws IOException { //Doing
+        PriorityBlockingQueue<PriorityProduct> pbq = new PriorityBlockingQueue<>(); //创建优先级阻塞队列时，若不指定队列容量，默认DEFAULT_INITIAL_CAPACITY = 1
 
         ExecutorService es = Executors.newFixedThreadPool(2);
         es.execute(new MyPriorityBlockingQueuePro(pbq, 1000));
@@ -113,7 +128,13 @@ public class BlockingQueueTest { //@MsY-Doing
          *
          * 结果分析：
          * 1）消费产品，按优先级大小消费队列中的产品
+         *
          * 2）看源码PriorityBlockingQueue#take时，取比较器来比较元素
+         *
+         * 3）new PriorityBlockingQueue<>() 创建优先级阻塞队列时，会进行如下的初始化
+         *    a）创建ReentrantLock、lock.newCondition()
+         *    b）指定比较器Comparator，若比较器为null，按自然排序
+         *    c）初始化队列元素new Object[initialCapacity]，若没有指定大小，默认为11
          */
     }
 
